e1ad0c44a26485fb175d44a700b625e46d3d0e65
José Alí Rivas Gómez <100250771+JoseAliRivasGomez@users.noreply.github.com>
2022-08-16 16:01:57 -0600

    test

--- middleware.ts
+++ middleware.ts
@@ -9,7 +9,7 @@ export async function middleware(req: NextRequest, ev: NextFetchEvent) {
 
         const session = await getToken({req, secret: process.env.NEXTAUTH_SECRET});
 
-        console.log({session});
+        console.log(process.env.NEXTAUTH_SECRET);
         
         if(!session){
             const { protocol, host, pathname } = req.nextUrl;
